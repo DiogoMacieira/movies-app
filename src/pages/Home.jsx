4b5553e22145ef781f45b99f7c338a9a3b97bce0
Diogo Macieira <diogo.andre.macieira@gmail.com>
2023-02-26 11:55:05 +0000

    .

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import { API_KEY } from "../services/api";
+import { movies } from "../services/api"; //vite_api
+// import {search} from "./services/api";
+// import {image} from "../services/api";
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async () => {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
+    );
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${movies}top_rated?${API_KEY}`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+  <div className="container">
+    <h2 className="title">Melhores filmes:</h2>
+    <div className="movies-container">
+        {/* {topMovies.length === 0 && <p>A carregar...</p>} */}
+{/*         {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)} */}
+    </div>
+</div>
+);
+};
+
+export default Home;

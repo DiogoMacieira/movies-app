711c25d0c0b98a15cd4319db7c6388f20e5044bb
Diogo Macieira <diogo.andre.macieira@gmail.com>
2023-03-01 19:27:36 +0000

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export const API_KEY =
   '4d92e46d5abd3840fc0bda4ac474badd';
 export const movies = "https://api.themoviedb.org/3/movie/";
-export const search = "https://api.themoviedb.org/3/seacrh/movie/";
+export const search = "https://api.themoviedb.org/3/search/movie";
 export const image =  "https://image.tmdb.org/t/p/w500";
 

3cafb2903e8fa2b68f7fcdd8950d58e6efc4f045
Diogo Macieira <diogo.andre.macieira@gmail.com>
2023-03-10 12:40:29 +0000

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import './pages/MoviesGrid.css';
-import App from './App';
-import Home from './pages/Home';
-import Movie from './pages/Movie';
-import Search from './pages/Search';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./pages/MoviesGrid.css";
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route element={<App/>}>
-      <Route path='/' element={<Home/>}/>
-      <Route path='movie/:id' element={<Movie/>}/>
-      <Route path='search' element={<Search/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+        ,
+        {{
+          basename: "/movies-app",
+        }}
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-
-  {
-    basename: "/movies-app"
-  }
+  </React.StrictMode>
 );
-
-
